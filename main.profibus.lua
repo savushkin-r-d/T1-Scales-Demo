23338a6096d46e5c2ca478132ef9329c47d8d572
Dzmitry Ivaniuk <dzimitriy@gmail.com>
2022-06-16 16:09:32 +0300

    Add project files

---
+++ main.profibus.lua
@@ -0,0 +1,7 @@
+--version  = 1
+--Eplanner version = 2022.6.8202.25836
+--Описание конфигурации PROFIBUS
+------------------------------------------------------------------------------
+system = system or { }
+system.init_profibus = function()
+end
---
+++ shared.lua
@@ -0,0 +1,7 @@
+--Eplanner version = 2022.6.8202.25836
+remote_gateways =
+{
+}
+shared_devices =
+{
+}
